f898f6a246f13415caf3e02a77a935fa74e8bacc
ImGunHyoeng <ynoe30@gmail.com>
2024-04-03 22:03:31 +0900

    solve 16953 use heap prim algorithm

--- 16953/16953/소스.cpp
+++ 16953/16953/소스.cpp
@@ -2,28 +2,42 @@
 
 using namespace std;
 
+struct Node
+{
+	Node(long long _value,int _count):value(_value),count(_count)
+	{
+	
+	}
+	Node():Node(0,0)
+	{
+	}
+	long long value;
+	int count;
+};
 class Heap
 {
-	int* array;
+	Node* array;
 	int count;
 	int size;
 public:
-	Heap():count(0),array(new int[10]), size(10)
+	Heap():count(0),array(new Node[3]), size(3)
 	{}
 	~Heap() { delete [] array; }
 
+	int getCount() { return count; }
 	void resize()
 	{
-		int* temp = new int[size * 2];
+		Node* temp = new Node[size * 2];
 		for (int i = 0; i < size; i++)
 			temp[i] = array[i];
 		array = temp;
+		size *= 2;
 	}
-	void add(int value)
+	void add(const Node * input)
 	{
-		if (count + 1 > size)
+		if (count >= size)
 			resize();
-		array[count] = value;
+		array[count] = *input;
 		count++;
 		heapSortBottomUp();
 	}
@@ -32,24 +46,24 @@ public:
 		if (count == 1)
 			return;
 		
-		int myvalue = array[count-1];
+		Node myNode = array[count-1];
 		int cur = count-1;
 
 		while (cur != 0)
 		{
-			if (array[cur] >= array[(cur - 1) / 2])
+			if (array[cur].value >= array[(cur - 1) / 2].value)
 				break;
 			array[cur] = array[(cur - 1) / 2];
 			cur = (cur - 1) / 2;
-			
+			//올바르게 고친것
+			array[cur] = myNode;
 		}
-		array[cur] = myvalue;
+
+		//array[cur] = myNode; 이의 방식으로 하면은 add로 부모와 자리 바꾸는 것을 한번밖에 못함
 	}
-	int pop()
+	const Node pop()
 	{
-		if (count == 0)
-			return -1;
-		int result = array[0];
+		Node result = array[0];
 		array[0] = array[count - 1];
 		count--;
 		heapSortTopDown();
@@ -57,7 +71,9 @@ public:
 	}
 	void heapSortTopDown()
 	{
-		int myvalue = array[0];
+		if (count == 1)
+			return;
+		Node myNode = array[0];
 		int left = 0, right = 0, cur=0;
 
 		while (1)
@@ -65,48 +81,77 @@ public:
 			left = cur * 2 + 1;
 			right = cur * 2 + 2;
 
-			if (left >= count || right >= count)
+			if (left >= count-1 && right >= count-1)
 				break;
-			if (array[cur] <= array[left] && array[cur] <= array[right])
-				break;
-
-			if (array[left] < array[right])
+			
+			//자식이 하나일 경우
+			if (left == count-1)
 			{
-				array[cur] = array[cur * 2 + 1];
-				cur = cur * 2 + 1;
+				if (array[cur].value <= array[left].value)
+					break;
+				array[cur] = array[left];
+				cur = left;
+				array[cur] = myNode;
 				continue;
 			}
 
-			if (array[left] > array[right])
+			//자식이 두개일 경우
+			if (array[cur].value <= array[left].value && array[cur].value <= array[right].value)
+				break;
+			if (array[left].value < array[right].value)
 			{
-				array[cur] = array[cur * 2 + 2];
-				cur = cur * 2 + 2;
+				array[cur] = array[left];
+				cur = left;
+				array[cur] = myNode;
+				continue;
+			}
+
+			if (array[left].value > array[right].value)
+			{
+				array[cur] = array[right];
+				cur = right;
+				array[cur] = myNode;
 				continue;
 			}
 		}
 		//값 할당
-		array[cur] = myvalue;
+
 	}
 };
+
 
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
+	long long start, goal;
+	cin >> start;
+	cin >> goal;
 
+	int count = -1;
 	Heap heap;
 
-	int* arr = new int[5];
-	for (int i = 0; i < 5; i++)
+	Node temp;
+	heap.add(new Node(start, 1 ));
+	while (heap.getCount() != 0)
 	{
-		cin >> arr[i];
-		heap.add(arr[i]);
+		temp = heap.pop();
+		if(temp.value*2<=goal)
+			heap.add(new Node(temp.value*2, temp.count + 1 ));
+		if (temp.value * 10 + 1 <= goal)
+			heap.add(new Node(temp.value*10+1, temp.count + 1 ));
+		if (temp.value == goal)
+		{
+			if (count == -1)
+			{
+				count = temp.count;
+				continue;
+			}
+			if (temp.count < count)
+				count = temp.count;
+		}
 	}
-
-	for (int i = 0; i < 5; i++)
-	{
-		cout<<heap.pop();
-	}
+	cout << count;
 	return 0;
 }
