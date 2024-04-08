786e04be329ebd028a9df23342aa39786e711317
ImGunHyoeng <ynoe30@gmail.com>
2024-04-09 02:43:13 +0900

    solve 29813

--- 29813/29813/소스.cpp
+++ 29813/29813/소스.cpp
@@ -1,65 +1,105 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-using namespace std;
-struct student
-{
-	const char* name;
-	int passable;
-	student* next;
-	student(const char *_name,int input):name(_name),passable(input)
-	{
-		next = nullptr;
-	}
-	student():student(nullptr,00)
-	{
+#include <cstring>
 
+
+using namespace std;
+class Student
+{
+public:
+	char * name;
+	int passable;
+	Student* next;
+	Student(const char *_name,int input): Student()
+	{
+		
+		passable=input;
+		if (input % 10 == 0)
+			passable = passable / 10;
+		name = new char[strlen(_name)+1];
+		strcpy(name, _name); 
+		
+	}
+	Student():name(nullptr), passable(0), next ( nullptr)
+	{
+		
+	}
+	~Student()
+	{
+		//delete [] name;
 	}
 };
 
-class CircleQueue
+class Queue
 {
-	student* front;
-	student* rear;
-	student* arr;
+	Student* front;
+	Student* rear;
+	Student* arr;
 	int curindex;
+	int size;
 public:
-	CircleQueue(int size)
+	Queue(int _size)
 	{
-		arr = new student[size];
+		arr = new Student[_size];
 		front = nullptr;
 		rear = front;
 		curindex = 0;
+		size = _size;
+	}
+	~Queue() 
+	{
+		//cout << "Quedelete" << endl;
+		front = arr;
+		for (int i = 0; i < size; i++)
+		{
+			//1.지역변수로 가면은 바로 초기화 되어서 안된다.
+			//front->~Student();
+			//2.큐가 소멸될 때 값을 초기화 시켜주는 것.
+			delete front->name;
+			//delete front;
+			front++;
+		}
+	
 	}
 
 	void add(const char *name,int value)
 	{
-		if (rear + 1 == front)
+		if (nullptr== front)
+		{
+			Student* temp = new Student(name, value);
+			arr = temp;
+			front = temp;
+			rear = temp;
+			curindex++;
 			return;
-		student *temp= new student(name, value);
-		rear = temp;
+		}
+		Student *temp= new Student(name, value);
+		arr[curindex] = *temp;
+		rear->next = temp;
+		rear++;
 		rear->next = NULL;
-		rear++; 
+		
 		curindex++;
 	}
-	student* indexpop(int index)
+
+	void change()
 	{
-		if (rear == front)
-			return NULL;
-		student* pre =front + index-1;
-		student *temp=front + index;
-		pre->next = temp->next;
-		const char* result =temp->name;
-		
-		curindex--;
-		return temp;
+		Student temp = *(front + 1);
+		Student* pt = front + 1;
+		while(pt!=rear)
+		{
+			*pt = Student((pt + 1)->name, (pt + 1)->passable);
+			pt++;
+			//arr[i] = Student(arr[i + 1].name, arr[i + 1].passable);
+		}
+		*rear = temp;
 	}
 	const char* frontpop()
 	{
-		if (rear == front)
-			return NULL;
-		student* temp = front;
+		Student* temp = front;
 		front++;
 		const char* result = temp->name;
-
+		
 		curindex--;
 		return result;
 	}
@@ -67,7 +107,7 @@ public:
 	{
 		return curindex;
 	}
-	student* getHead() 
+	Student* getHead() 
 	{
 		return front;
 	}
@@ -79,9 +119,9 @@ int main()
 	ios_base::sync_with_stdio(false);
 	int N;
 	cin >> N;
-	CircleQueue queue(N);
+	Queue queue(N);
 
-	char Name[4];
+	char *Name=new char[4];
 	int value;
 
 	for (int i = 0; i < N; i++)
@@ -90,21 +130,21 @@ int main()
 		cin >> value;
 		queue.add(Name, value);
 	}
-
+		
 	while (queue.cursize() != 1)
 	{
-		if (queue.getHead()->passable == 0)
+		if (queue.getHead()->passable == 1)
 		{
 			queue.frontpop();
-			queue.indexpop(1);
+			queue.frontpop();
 		}
 		else
 		{
 			queue.getHead()->passable--;
-			student *temp=queue.indexpop(1);
-			queue.add(temp->name,temp->passable);
+			queue.change();
 		}
 	}
-	cout << queue.frontpop();
+	cout<< queue.frontpop();
+
 	return 0;
 }
