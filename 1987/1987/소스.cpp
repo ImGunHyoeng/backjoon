d3d01f01319f17e5820dd84802ca88090a6ac752
ImGunHyoeng <ynoe30@gmail.com>
2024-05-08 00:53:25 +0900

    solving 1987

---
+++ 1987/1987/소스.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	char Arr[20][20] = {0,};
+	bool visited[26] = { 0, };
+
+	int R;
+	int C;
+	cin >> R;
+	cin >> C;
+	int Colcount = 0;
+	int Rowcount = 0;
+	while (1)
+	{
+		cin >> Arr[Rowcount][Colcount];
+		if (R * C == (Colcount + 1) * (Rowcount + 1))
+			break;
+		if (Colcount + 1 == C)
+		{
+			Rowcount++;
+			Colcount = 0;
+			continue;
+		}
+			Colcount++;
+	}
+	visited[Arr[0][0] - 65] = true;//¾ËÆÄºª È®ÀÎ
+
+
+	return 0;
+}
